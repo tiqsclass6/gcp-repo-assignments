6cd84f4193e2e32a3cd72dad125fed5c59eef469
tiqsclass6 <daquietstorm22@gmail.com>
2025-11-10 20:42:27 -0800

    fourth commit

--- 0-authentication.tf
+++ 0-authentication.tf
@@ -1,7 +1,7 @@
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs
 
 provider "google" {
-  project     = "your-project-id" # Insert your project id here
+  project     = "your-project-id"           # Insert your project id here
   region      = "us-central1"
-  credentials = "yourjsoncert.json" # Insert your json credential here
+  credentials = "yourjsoncert.json"         # Insert your json credential here
 }
--- 1-backend.tf
+++ 1-backend.tf
@@ -1,10 +1,11 @@
 # https://www.terraform.io/language/settings/backends/gcs
 terraform {
   backend "gcs" {
-    bucket      = "your-project-id" # Insert your project ID here
-    prefix      = "terraform/state"
-    credentials = "yourjsoncert.json" # Insert your json credential here
+    bucket      = "your-project-id"           # Insert your project ID here
+    prefix      = "MMDDYYYY/terraform/state"
+    credentials = "yourjsoncert.json"         # Insert your json credential here
   }
+
   required_providers {
     google = {
       source  = "hashicorp/google"
